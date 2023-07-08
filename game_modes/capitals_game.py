5778d8a419ed0b485b341118a91f375b2ce03f4a
ubbeck <60904946+ubbeck@users.noreply.github.com>
2023-07-08 15:15:27 +0200

    Modified the way random questions are generated
    Questions are generated from a list of randomly ordered integers (length equal to the list of countries). This list serves as an index to traverse the list of countries.

--- game_modes/capitals_game.py
+++ game_modes/capitals_game.py
@@ -1,10 +1,10 @@
 import telebot
 from telebot.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from feedback.feedback import check_hits
 
 import random
 import threading
 import time
+import re
 
 from countries_data import countries
 
@@ -12,41 +12,24 @@ NUM_COUNTRIES = len(countries)
 TIME_LIMIT = 5
 
 def play_capitals(message, bot: telebot, user: dict) -> None:
-    
+
     user['hits'] = 0
-    user['playedInThisRound'].clear()
-    bot.send_message(message.chat.id, "How many capitals can you guess?")
+    user['index'].clear()
+    user['index'] = list(range(0, NUM_COUNTRIES))
+    random.shuffle(user['index'])
+    bot.send_message(message.chat.id, "Capitals")
     next_question(message, bot, user)
-
-def get_candidates() -> list:
     
-    candidates = []
-    index = random.sample(range(NUM_COUNTRIES), 2)
-    for i in index:
-        candidates.append(countries[i]['city'])
-    return candidates
-
-def no_played_country(user: dict) -> dict:
-    
-    country = countries[random.randint(0, NUM_COUNTRIES - 1)]
-    while(country['country'] in user['playedInThisRound']):
-        country = countries[random.randint(0, NUM_COUNTRIES - 1)]
-    return country
-
-def check_answer(message, bot: telebot, user: dict, city: str):
+def check_answer(message, bot: telebot, user: dict, city: str) -> None:
     
     if 'timer' not in user:
         next_question(message, bot, user)
         return
     
-    elif message.text == "/back":
+    elif re.match(r'^/', message.text):
         user['timer'].cancel()
         user.pop('timer')
-        user['hits'] = 0
-        user['playedInThisRound'].clear()
-        bot.send_message(message.chat.id, "Exiting Capitals Contest Mode", reply_markup = ReplyKeyboardRemove())
-        time.sleep(1)
-        bot.send_message(message.chat.id, "Select new Game Mode from Menu")
+        bot.send_message(message.chat.id, "Exiting capitals contest mode", reply_markup = ReplyKeyboardRemove())
         return
 
     elif message.text == city:
@@ -54,7 +37,11 @@ def check_answer(message, bot: telebot, user: dict, city: str):
         bot.send_chat_action(message.chat.id, "typing")
         bot.send_message(message.chat.id, '<b><i>CORRECT</i></b>', parse_mode="html", disable_web_page_preview= True)
         user['hits'] += 1
-        next_question(message, bot, user)
+        if(user['hits'] == NUM_COUNTRIES):
+            bot.send_message(message.chat.id, ("&lt---------- " + '<b><i>PERFECT ROUND </i></b>' + " ----------&gt"), parse_mode= "html", disable_web_page_preview= True)
+            # finish game mode
+        else:
+            next_question(message, bot, user)
     else:
         user['timer'].cancel()
         bot.send_chat_action(message.chat.id, "typing")
@@ -66,34 +53,27 @@ def check_answer(message, bot: telebot, user: dict, city: str):
         #check_hits(message, bot, user)
         bot.send_message(message.chat.id, "Use /capitals to play again")
 
-def next_question(message, bot: telebot, user: dict):
-    
-    candidates = get_candidates()
-    country = no_played_country(user)
+def next_question(message, bot: telebot, user: dict) -> None:
 
-    while(country['city'] in candidates):
-        no_played_country(user)
-    
-    user['playedInThisRound'].append(country['country'])
-
-    candidates.append(country['city'])
-
+    country = user['index'][user['hits']]
+    aux_index = [i for i in user['index'] if i != country]
+    candidates = random.choices(aux_index, k=2) + [country]
     random.shuffle(candidates)
 
+    buttons_text = [countries[candidate]['city'] for candidate in candidates]
+
     markup = ReplyKeyboardMarkup(
-        one_time_keyboard= False,
-        input_field_placeholder= "Choose One",
-        resize_keyboard= False,
-        row_width=1
+        one_time_keyboard = False,
+        input_field_placeholder = "Choose One",
+        resize_keyboard = False,
+        row_width = 1
     )
 
-    markup.add(candidates[0], candidates[1], candidates[2])
-
-    bot.send_chat_action(message.chat.id, "typing")
+    markup.add(*buttons_text)
     user['timer'] = threading.Timer(TIME_LIMIT, handle_timeout, args=[message, bot, user])
     user['timer'].start()
-    answer = bot.send_message(message.chat.id, (f"{country['country']}" + " " + f"{country['flag'].encode().decode('unicode_escape')}"),reply_markup=markup)
-    bot.register_next_step_handler(answer, check_answer, bot, user, country['city'])
+    answer = bot.send_message(message.chat.id, (f"{countries[country]['country']}" + " " + f"{countries[country]['flag'].encode().decode('unicode_escape')}"),reply_markup=markup)
+    bot.register_next_step_handler(answer, check_answer, bot, user, countries[country]['city'])
 
 def handle_timeout(message, bot: telebot, user: dict):
     
@@ -101,5 +81,5 @@ def handle_timeout(message, bot: telebot, user: dict):
     user.pop('timer')
     bot.send_message(message.chat.id, "Lose By Time Out", reply_markup = ReplyKeyboardRemove())
     user['hits'] = 0
-    user['playedInThisRound'].clear()
+    user['index'].clear()
     bot.send_message(message.chat.id, "Use /capitals to play again")
